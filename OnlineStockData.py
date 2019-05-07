92990a559d821c726f8fc045c82663b1ba46fe26
tn16jv <tn16jv@brocku.ca>
2019-05-06 22:26:06 -0400

    Added string formatting

--- OnlineStockData.py
+++ OnlineStockData.py
@@ -12,4 +12,4 @@ def get_stock_data(ticker=None, start_date=None, end_date=None):
         except:
             print("Invalid, try again.")
             ticker = input("Enter a stock ticker: ")
-    return data.reset_index()
+    return data.reset_index(), ticker
